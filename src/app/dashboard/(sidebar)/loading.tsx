cd2c6938b9361ecad0036c7c7ce9d07984dd14da
Yagnik Patel <coderisaddicted@gmail.com>
2023-07-27 23:32:05 +0530

    responsive sidebar for dashboard page

--- src/app/dashboard/(sidebar)/loading.tsx
+++ src/app/dashboard/(sidebar)/loading.tsx
@@ -1,5 +1,5 @@
 import LoadingSpinner from "@/components/loading-spinner"
 
 export default function DashboardLoading() {
-  return <LoadingSpinner sizeStyle="w-16 h-16" widthStyle="basis-[80%]" />
+  return <LoadingSpinner sizeStyle="w-16 h-16" widthStyle="lg:basis-[80%]" />
 }
