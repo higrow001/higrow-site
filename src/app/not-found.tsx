e9d44e71f8d0da3a4f5d63de56d253fff98cd264
Puneet Kathuria <puneetkathuria2525@gmail.com>
2023-08-08 23:14:20 +0530

    Roughhhh

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,9 +7,9 @@ export default function notFound() {
       <h1 className="font-archivo font-bold text-center text-4xl lg:text-6xl">Whoops, that page is Gone.</h1>
       <p className="text-base md:text-xl text-secondary text-center font-medium lg:w-[50ch]">We searched everywhere but couldn't find what you're looking for.
         Let's find a better place for you to go.</p>
-      <Link href="/">
-        <Button className="px-6" size={"xl"}>Back to Home</Button>
-      </Link>
+      
+        <Button className="px-6" size={"xl"}>  <Link href="/" className="w-full h-full">Back to Home </Link></Button>
+      
     </main>
   )
 }
