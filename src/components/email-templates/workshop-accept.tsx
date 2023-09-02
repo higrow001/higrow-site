147912dcf66776794a26a8b9073c2b2cd66e125b
Yagnik Patel <coderisaddicted@gmail.com>
2023-09-02 23:06:36 +0530

    email templates edit

--- src/components/email-templates/workshop-accept.tsx
+++ src/components/email-templates/workshop-accept.tsx
@@ -3,22 +3,18 @@ import {
   Body,
   Button,
   Container,
-  Column,
   Head,
   Heading,
-  Hr,
   Html,
-  Img,
-  Link,
   Preview,
-  Row,
   Section,
   Tailwind,
   Text,
-} from '@react-email/components';
+} from "@react-email/components"
+
 interface EmailTemplateProps {
-  workshopName: string;
-  pageLink: string;
+  workshopName: string
+  pageLink: string
 }
 
 export const WorkshopAcceptTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
@@ -26,43 +22,41 @@ export const WorkshopAcceptTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   pageLink,
 }) => (
   <Html>
-  <Head />
-  <Preview>We accepted your workshop</Preview>
-  <Tailwind>
-  <Body className="bg-white my-auto mx-auto font-sans">
-  <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] w-[465px]">
-  <Section>
-    <Heading className="text-[32px] font-archivo text-black text-center font-bold"> HiGrow. </Heading>
-  </Section>
-
-  <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-  Your workshops is accepted!
+    <Head />
+    <Preview>We accepted your workshop</Preview>
+    <Tailwind>
+      <Body className="bg-white my-auto mx-auto font-sans">
+        <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] w-[465px]">
+          <Section>
+            <Heading className="text-[32px] font-archivo text-black text-center font-bold">
+              {" "}
+              HiGrow.{" "}
             </Heading>
-            <Text className="text-black text-[14px] leading-[24px]">
-              Hey,
-            </Text>
-            <Text className="text-black text-[14px] leading-[24px]">
-              Congratulations! You're workshop "{workshopName}" is approved by out team! See participants and make announcements now!
-              </Text>
-              <Button
-                pX={20}
-                pY={12}
-                className="bg-[#000000] my-auto mx-auto mt-2 mb-2 rounded text-white text-[12px] font-semibold no-underline text-center"
-                href={pageLink}
-              >
-              Check Now
-              </Button>
-              <Text className="text-black text-[14px] leading-[24px]">
-              or copy and paste this URL into your browser:{' '}
-              <a
-                href={pageLink}
-                className="text-blue-600 no-underline"
-              >
-                {pageLink}
-              </a>
-            </Text>
-    </Container>
-  </Body>
-  </Tailwind>
-</Html>
+          </Section>
+          <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
+            Your workshops is accepted!
+          </Heading>
+          <Text className="text-black text-[14px] leading-[24px]">Hey,</Text>
+          <Text className="text-black text-[14px] leading-[24px]">
+            Congratulations! You're workshop "{workshopName}" is approved by out
+            team! See participants and make announcements now!
+          </Text>
+          <Button
+            pX={20}
+            pY={12}
+            className="bg-[#000000] my-auto mx-auto mt-2 mb-2 rounded text-white text-[12px] font-semibold no-underline text-center"
+            href={pageLink}
+          >
+            Check Now
+          </Button>
+          <Text className="text-black text-[14px] leading-[24px]">
+            or copy and paste this URL into your browser:{" "}
+            <a href={pageLink} className="text-blue-600 no-underline">
+              {pageLink}
+            </a>
+          </Text>
+        </Container>
+      </Body>
+    </Tailwind>
+  </Html>
 )
